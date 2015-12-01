e30ea32e08005a56b63d9d0c5e7da462a44c393c
LM-G <louismarie.guillemot@gmail.fr>
2015-12-01 17:42:44 +0100

    ajout vue about

---
+++ views/about/about-module.js
@@ -0,0 +1,8 @@
+(function () {
+	'use strict';
+	console.log('Chargement du module about ...');
+
+	angular.module('funApp.views.about', []);
+	
+	console.log('Modules about charge.');
+})();
---
+++ views/about/about-state.js
@@ -0,0 +1,27 @@
+/**
+ * Chargement des directives et états de la page about
+ */
+(function() {
+	'use strict';
+	angular.module('funApp.views.about')
+		.constant('aboutConfig', {
+			nom : "about",
+			type : "view",
+			config : {
+		        url: "/about",
+		        templateUrl: "/views/about/about.html"
+	    	}
+	    })
+		.config([
+			'$stateProvider',
+			'aboutConfig',
+			configuration
+		]);
+
+	function configuration($stateProvider, aboutConfig) {
+		$stateProvider
+			.state(aboutConfig.nom, aboutConfig.config);
+
+		console.info("config about charge");
+	}
+})();
